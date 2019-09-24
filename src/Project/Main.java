3999847d0a2b72079e77db3fa3ebfa8fd34fbafa
Boyd Smit <boydsmit@live.nl>
2019-09-24 22:46:22 +0200

    =Rewrote the algorithm to generate the roman numerals +Added comments/documentation

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -1,109 +1,76 @@
 package Project;
+
 import java.util.Scanner;
 
 public class Main {
-    private static double number;
-    private static String romanString = "";
-    enum RomaNumerals{
-        M, D, C, L, X, V, I
-    }
-
 
     public static void main(String[] args) {
         try {
             //Gets the input from user
-            Scanner input = new Scanner(System.in);
-            number = input.nextDouble();
-
-            while(number != 0) {
-                findCorrectNumeral();
-            }
-            System.out.println(romanString);
-
+            final Scanner input = new Scanner(System.in);
+            //Converts the given input to roman numerals.
+            final String romanNumeral = getNumeral(input.nextInt());
+            //Prints the final string
+            System.out.print(romanNumeral);
         } catch (Exception e) {
-            System.out.println("Could not find an integer. Are you sure you used a number? \nPlease try again.");
+            System.out.print("Could not find an integer. Are you sure you used a number? \nPlease try again.");
         }
     }
 
-    private static void findCorrectNumeral() {
-        if (number - 1000  >= 0) {
-            romanString = romanString + "M";
-            number -= 1000;
+    /**
+     * Loops through the {@link RomanNumeral} enum to find the correct numeral.
+     *
+     * @param number        The {@link Integer} required for conversion.
+     * @return              A {@link String} of roman numerals.
+     */
+    private static String getNumeral(int number) {
+        //String builder is used to append multiple string instead of using a single string.
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (number > 0) {
+            //Loops through all the enum values
+            for (RomanNumeral value : RomanNumeral.values()) {
+                //Checks if the current enum value is possible
+                if (number - value.getNumber() >= 0) {
+                    //Adds the Adds te roman numeral of the found possible number to the string builder.
+                    stringBuilder.append(value.name());
+                    number -= value.getNumber();
+                    break;
+                }
+            }
         }
-        else if(number - 900 >= 0) {
-            romanString = romanString + "CM";
-            number -= 900;
+        //Returns the final string.
+        return stringBuilder.toString();
+    }
+
+        public enum RomanNumeral {
+            //Order of values is important!
+            M(1000),
+            CM(900),
+            D(500),
+            CD(400),
+            C(100),
+            XC(90),
+            L(50),
+            XL(40),
+            X(10),
+            IX(9),
+            V(5),
+            IV(4),
+            I(1);
+
+        private final int number;
+
+        //The constructor of the enum.
+        RomanNumeral(int number) {
+            this.number = number;
         }
-        else if(number - 500  >= 0) {
-            romanString = romanString + "D";
-            number -= 500;
-        }
-        else if(number - 400 >= 0 ) {
-            romanString = romanString + "CD";
-            number -= 400;
-        }
-        else if(number - 100 >= 0) {
-            romanString = romanString + "C";
-            number -= 100;
-        }
-        else if(number - 90 >= 0) {
-            romanString = romanString + "XC";
-            number -= 90;
-        }
-        else if (number - 50 >= 0) {
-            romanString = romanString + "L";
-            number -= 50;
-        }
-        else if(number - 40 >= 0) {
-            romanString = romanString + "XL";
-            number -= 40;
-        }
-        else if(number - 10 >= 0) {
-            romanString = romanString + "X";
-            number -= 10;
-        }
-        else if(number - 9 >= 0) {
-            romanString = romanString + "IX";
-            number -= 9;
-        }
-        else if(number - 5 >= 0) {
-            romanString = romanString + "V";
-            number -= 5;
-        }
-        else if(number - 4 >= 0) {
-            romanString = romanString + "IV";
-            number -= 4;
-        }
-        else if(number - 1 >= 0) {
-            romanString = romanString + "I";
-            number -= 1;
+
+        //Will return the number of the current value.
+        public int getNumber() {
+            return number;
         }
     }
 
-    private static void findNumerals2() {
-         RomaNumerals CurNumeral;
-        CurNumeral = RomaNumerals.M;
-        switch(CurNumeral){
-            case M:
-
-                break;
-            case D:
-
-                break;
-            case C:
-
-                break;
-            case L:
-
-                break;
-            case X:
-
-                break;
-            case V:
-                break;
-            case I:
-                break;
-        }
-    }
 }
 
